f29fe43f7af7f776d9dc52d07011adce6f31294f
MFischmann <micael.fischmann@gmail.com>
2019-11-08 16:49:59 -0200

    :sparkles: Cria classe Deque

---
+++ Deque.java
@@ -0,0 +1,21 @@
+public class Deque{
+    DoubleLinkedListOfCards deck;
+
+    public Deque(){
+        deck = new DoubleLinkedListOfCards();
+    }
+    /**
+     * Retorna uma carta aleatoria de deck
+     * @return A carta aleatoria
+     */
+    public Card compraCard(){
+        //TODO
+    }
+    /**
+     * Retorna uma string contendo a informacao de todas as cartas contidas em deck
+     * @return s
+     */
+    public String printDeque(){
+        //TODO
+    }
+}
