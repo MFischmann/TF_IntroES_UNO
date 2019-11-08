d6403fa97f5be1c1c63ac30f8c68c5c12bc81ffb
Micael Fischmann <micael.fischmann@agst.com.br>
2019-11-08 15:40:57 -0200

    :sparkles: Cria chamada para metodos de  DoubleLinkedListOfCards

--- DoubleLinkedListOfCards.java
+++ DoubleLinkedListOfCards.java
@@ -1,5 +1,7 @@
 public class DoubleLinkedListOfCards{
-
+    private Node header;
+    private Node trailer;
+    int count;
 
     private class Node {
         public Card element;
@@ -10,5 +12,52 @@ public class DoubleLinkedListOfCards{
             next = null;
             prev = null;
         }
+    }
+
+    public DoubleLinkedListOfCards(){
+        header = new Node(null);
+        trailer = new Node(null);
+        header.next = trailer;
+        trailer.prev = header;
+        count = 0;
+    }
+    /**
+     * Adiciona Card element no fim da lista
+     * @param element
+     */
+    public void add(Card element){
+        //TODO
+    }
+    /**
+     * Remove Card da posicao index e a retorna
+     * @param index posicao da Card a ser removida
+     * @return a Card removida
+     * @throws IndexOutOfBoundsException se posicao inexiste
+     */
+    public Card remove(int index){
+        //TODO
+        if(index < 0 || index > count){
+            throw new IndexOutOfBoundsException("Valor invalido de index.");
+        }
+    }
+
+        /**
+     * Retorna Card da posicao index
+     * @param index posicao da Card a ser retornada
+     * @return a Card removida
+     * @throws IndexOutOfBoundsException se posicao inexiste
+     */
+    public Card getCard(int index){
+        //TODO
+        if(index < 0 || index > count){
+            throw new IndexOutOfBoundsException("Valor invalido de index.");
+        }
+    }
+    /**
+     * Retorna tamanho da lista
+     * @return tamanho da lista
+     */
+    public int size(){
+        return count;
     }
 }
