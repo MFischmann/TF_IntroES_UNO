c193ba3e119646e8725ce65b3449db9a0ce19333
Micael Fischmann <micael.fischmann@agst.com.br>
2019-11-08 15:27:10 -0200

    :sparkles: Adiciona arquivo Card e metodos

---
+++ Card.java
@@ -0,0 +1,23 @@
+public class Card{
+    private String valor;
+    private String cor;
+
+    public Card(String valor, String cor){
+        this.valor = valor;
+        this.cor = cor;
+    }
+
+    /**
+     * @return valor
+     */
+    public String getValor(){
+        return valor;
+    }
+
+    /**
+     * @return cor
+     */
+    public String getCor() {
+        return cor;
+    }
+}
