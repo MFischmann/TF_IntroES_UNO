cac941fc1a6ef5cb41bceae284d4cce74ae87db3
MFischmann <micael.fischmann@gmail.com>
2019-11-11 09:35:07 -0200

    :sparkles: Adiciona metodos de Jogador

--- Jogador.java
+++ Jogador.java
@@ -1,4 +1,17 @@
 public class Jogador{
     private String nome;
     private DoubleLinkedListOfCards hand;
+
+    public Jogador(String nome){
+        this.nome = nome;
+        hand = new DoubleLinkedListOfCards();
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public DoubleLinkedListOfCards getHand(){
+        return hand;
+    }
 }
