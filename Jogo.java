0d41958630f79251a55a702cab4aa9e5f829e5bd
MFischmann <micael.fischmann@gmail.com>
2019-11-11 10:04:42 -0200

    :sparkles: Adiciona metodos get de Jogo

--- Jogo.java
+++ Jogo.java
@@ -12,4 +12,31 @@ public class Jogo{
         ID = IDcounter;
         IDcounter++;
     }
+
+    /**
+     * @return o deck
+     */
+    public Deque getDeck() {
+        return deck;
+    }
+
+    /**
+     * @return A ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @return Lista jogadores
+     */
+    public DoubleLinkedListOfPlayers getJogadores() {
+        return jogadores;
+    }
+    /**
+     * @return true se e ordem normal de jogo e false se a ordem estiver invertida
+     */
+    public boolean getOrdem(){
+        return ordemNormal;
+    }
 }
