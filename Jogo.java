113afb1532b7582138e91b8d154c248687a62340
MFischmann <micael.fischmann@gmail.com>
2019-11-11 10:02:00 -0200

    :sparkles: Adiciona construtor de Jogo

--- Jogo.java
+++ Jogo.java
@@ -1,7 +1,15 @@
 public class Jogo{
     private Deque deck;
-    private static int IDcounter;
+    private static int IDcounter = 1;
     private int ID;
     private DoubleLinkedListOfPlayers jogadores;
     private boolean ordemNormal;
+
+    public Jogo(){
+        deck = new Deque();
+        jogadores = new DoubleLinkedListOfPlayers();
+        ordemNormal = true;
+        ID = IDcounter;
+        IDcounter++;
+    }
 }
